eee780ead87480f71d308563d4c51de1cc1f4b64
ZacharyTa <Zachary.C.Ta@gmail.com>
2024-10-31 13:08:08 -0700

    Signin page's funcitoality now only redirects user to work page if they are an employee

--- src/app/signin/page.tsx
+++ src/app/signin/page.tsx
@@ -1,14 +1,37 @@
+"use client";
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+
 export default function SignInPage() {
+  const router = useRouter();
+  const [name, setName] = useState<string>("");
+
+  const handleSignIn = () => {
+    if (name?.toLocaleLowerCase() === "zachary") router.push("/work");
+  };
+
   return (
     <div className="h-screen flex items-center justify-center text-4xl bg-base-100">
       <div className="flex-col text-start space-y-4 outline outline-2 rounded-md p-10 bg-base-content">
         <p className="text-center">Sign In</p>
         <label className="input input-bordered flex items-center gap-2 text-black">
           Name
-          <input type="text" className="grow" placeholder="John" />
+          <input
+            type="text"
+            className="grow"
+            placeholder="John"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
         </label>
         <div className="flex justify-center">
-          <button className="btn btn-outline btn-primary">Sign in</button>
+          <button
+            className="btn btn-outline btn-primary"
+            onClick={handleSignIn}
+          >
+            {" "}
+            Sign in
+          </button>
         </div>
       </div>
     </div>

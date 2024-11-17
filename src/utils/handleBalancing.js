8e8acc726394519a226d978aac9f2332d1b18d03
ZacharyTa <Zachary.C.Ta@gmail.com>
2024-11-17 14:11:51 -0800

    updated documentaiton

--- src/utils/handleBalancing.js
+++ src/utils/handleBalancing.js
@@ -1,33 +1,38 @@
 // logic for handling the A* search algo smth
 
 export default function handleBalancing(manifestText) {
-    const newManifestText = manifestText;
-    // Note: right now, all this does is return the manifest text without any changes
-    // This function will handle balancing the containers in the manifestText
-    // The data format of the manifestText is a string:
+    const optimalOperations = [
+        {name: "Ram", time: 4, oldRow: 1, oldColumn: 4, newRow: 1, newColumn: 8},
+        {name: "Cat", time: 1, oldRow: 1, oldColumn: 4, newRow: 2, newColumn: 4},
+        {name: "Dog", time: 2, oldRow: 1, oldColumn: 4, newRow: 2, newColumn: 5},
+    ];
+    // Note: right now, all this does is return the list of random operations
+    // This function will handle loading/unloading the containers. 
+    // The data format of argument: manifestText <string>:
     // [row, col], {weight}, item
     // ...
     // [01, 02], {00000}, NAN
     // [01, 03], {00000}, NAN
     // [01, 04], {00120}, Ram
     // ....
-    
+
     // --------------------------------------------------
     // TODO: Implement ur search algorithm logic stuff here
-    // ur code here
+    // ur code here (Remember to record ur screens!)
     
 
 
 
     // --------------------------------------------------
 
-    // Return requirements:
-    // Data format: Ensure that the newManifestText is the same exactly format as the manifestText:
-    // [row, col], {weight}, item
-    // ...
-    // [01, 02], {00120}, Ram // Switched from NAN to Ram
-    // [01, 03], {00000}, NAN
-    // [01, 04], {00000}, NAN // Switched from Ram to NAN
-    // ....
-    return newManifestText;
+   // Return requirements:
+    // List of operations the user should perform in order (each operation should have an estimated execution time in minutes)
+    // Data format:
+    // [{name, time, oldRow, oldColumn, newRow, newColumn}, ...]
+    // [...
+    // {name: "Ram", time: 4, oldRow: 1, oldColumn: 4, newRow: 1, newColumn: 8},
+    // {name: "Cat", time: 1, oldRow: 1, oldColumn: 4, newRow: 2, newColumn: 4},
+    // {name: "Dog", time: 2, oldRow: 1, oldColumn: 4, newRow: 2, newColumn: 5},
+    // ....]
+    return optimalOperations;
 }
--- src/utils/handleLoading.js
+++ src/utils/handleLoading.js
@@ -1,20 +1,31 @@
 // logic for handling the A* search algo smth
 
-export default function handleLoading(manifestText) {
-    const newManifestText = manifestText;
-    // Note: right now, all this does is return the manifest text without any changes
-    // This function will handle loading/unloading the containers in the manifestText
-    // The data format of the manifestText is a string:
+export default function handleLoading(manifestText, operations) {
+    const optimalOperations = operations;
+    // Note: right now, all this does is return the list of operations without any changes
+    // This function will handle loading/unloading the containers. 
+    // The data format of argument: manifestText <string>:
     // [row, col], {weight}, item
     // ...
     // [01, 02], {00000}, NAN
     // [01, 03], {00000}, NAN
     // [01, 04], {00120}, Ram
     // ....
+
+    // Operations contain a list of onloading/offloading operations the user wants to perform
+    // The data format of argument: operations
+    // [{type, name}, {type, name}, ...]
+    // [
+    // ...
+    // {type: "onload", name: "Dog"},
+    // {type: "onload", name: "Cat"},
+    // {type: "offload", name: "Beans"},
+    // ...
+    // ]
     
     // --------------------------------------------------
     // TODO: Implement ur search algorithm logic stuff here
-    // ur code here
+    // ur code here (Remember to record ur screens!)
     
 
 
@@ -22,12 +33,13 @@ export default function handleLoading(manifestText) {
     // --------------------------------------------------
 
     // Return requirements:
-    // Data format: Ensure that the newManifestText is the same exactly format as the manifestText:
-    // [row, col], {weight}, item
-    // ...
-    // [01, 02], {00120}, Ram // Switched from NAN to Ram
-    // [01, 03], {00000}, NAN
-    // [01, 04], {00000}, NAN // Switched from Ram to NAN
-    // ....
-    return newManifestText;
+    // List of operations the user should perform in order (each operation should have an estimated execution time in minutes)
+    // Data format:
+    // [{type, name, time, row, column}, ...]
+    // [... 
+    // {type: "onload", name: "Dog", time: 12, row: 1, column: 12},
+    // {type: "offload", name: "beans", time: 9, row: 0, column: 0}, // For offload operations, set row/column to 0
+    // {type: "onload", name: "Cat", time: 19, row: 2, column: 12},
+    // ....]
+    return optimalOperations;
 }
